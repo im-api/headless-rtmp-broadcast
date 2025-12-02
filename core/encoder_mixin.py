27737a249cf5fb44525d9c8ef3ecd4a04ce46bf5
M3H_T <42382118+im-api@users.noreply.github.com>
2025-12-02 08:10:37 +0330

    .

--- core/encoder_mixin.py
+++ core/encoder_mixin.py
@@ -82,8 +82,6 @@ class EncoderMixin:
 
             # ⚠️ IMPORTANT: help ffmpeg handle the UDP H.264 TS stream
             "-fflags", "+nobuffer+genpts",
-            "-analyzeduration", "10M",
-            "-probesize", "10M",
 
             # Video from Stream B over UDP (MPEG-TS)
             "-f", "mpegts",
